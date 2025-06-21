9cb7b3d53e602c49158363ce589454a1225b571e
D Madan Mohan Rao <donkamadanmohanrao@gmail.com>
2025-06-21 18:54:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+window.addEventListener("scroll", () => {
+  const scrollY = window.scrollY;
+  const body = document.body;
+  const navbar = document.getElementById("navbar");
+  const logo = document.querySelector(".logo");
+  const topBtn = document.getElementById("toTop");
+
+  if (scrollY > 150) {
+    body.style.backgroundColor = "#fff8e1"; // light court tone
+    navbar.classList.add("scrolled");
+    logo.style.transform = "scale(1.1)";
+    logo.style.color = "#ffcc80";
+    topBtn.style.display = "block";
+  } else {
+    body.style.backgroundColor = "#fdfdfb";
+    navbar.classList.remove("scrolled");
+    logo.style.transform = "scale(1)";
+    logo.style.color = "#ffffff";
+    topBtn.style.display = "none";
+  }
+});
+
+// Scroll to top when button clicked
+document.getElementById("toTop").addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
